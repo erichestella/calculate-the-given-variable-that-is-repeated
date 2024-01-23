61fbb19a07f96ac64c83d45c5b924fbc23ae5841
erichestella <erichestella05@gmail.com>
2024-01-23 08:29:47 +0800

    variable to use for counting the given

--- counting repeating variable.py
+++ counting repeating variable.py
@@ -3,3 +3,7 @@ print('\nCOUNTING THE VARIABLE THAT IS REPEATING:')
 
 #putting some display 
 print('\nGiven: Nicole is such a cutie. Nicole is soft masc. Nicole is shorter than me. Nicole is very funny person. \nNicole is my crush in BSCPE 1-4. Nicole likes to go to different places. Nicole is very pretty. Nicole is smart. \nNicole is my one and lowkey crush.\n ')
+
+#these are the variable  
+type_name = 'Nicole is such a cutie. Nicole is soft masc. Nicole is shorter than me. Nicole is very funny person. \nNicole is my crush in BSCPE 1-4. Nicole likes to go to different places. Nicole is very pretty. Nicole is smart. \nNicole is my one and lowkey crush.\n '
+find = 'Nicole' 
