cff14ef6d40e3eb9a3d20530dbceab9b2012ca56
erichestella <erichestella05@gmail.com>
2024-01-23 08:31:14 +0800

    variable to use that needs to defines how many are being counted

--- counting repeating variable.py
+++ counting repeating variable.py
@@ -7,3 +7,7 @@ print('\nGiven: Nicole is such a cutie. Nicole is soft masc. Nicole is shorter t
 #these are the variable  
 type_name = 'Nicole is such a cutie. Nicole is soft masc. Nicole is shorter than me. Nicole is very funny person. \nNicole is my crush in BSCPE 1-4. Nicole likes to go to different places. Nicole is very pretty. Nicole is smart. \nNicole is my one and lowkey crush.\n '
 find = 'Nicole' 
+
+#these are the variable that defines the counting of the variable 
+counting_variable = 0 
+start = 0
